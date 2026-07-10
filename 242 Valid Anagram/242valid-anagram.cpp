b670bd39dde1ca9288535c69f93a90b6b46f15dd
Ishit Duggal <Ishitaduggal22@gmail.com>
2026-07-10 17:35:33 +0000

    Code challenge submission for question: 242 Valid Anagram

---
+++ 242 Valid Anagram/242valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+        {
+            return false;
+        }
+
+        map<char,int> map1, map2;
+
+        for(int i=0; i<s.length();i++)
+        {
+            map1[s[i]]++;
+            map2[t[i]]++;
+        }
+
+        if(map1 == map2)
+        {
+            return true;
+        }
+        return false;
+    }
+};
